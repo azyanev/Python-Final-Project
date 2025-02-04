0851acaf106d718a4a684e9601e5ea1e5efda19c
AZYanev18 <azyanev@uni-sofia.bg>
2025-02-05 00:43:16 +0200

    Submission of posts

--- finalproject/app/routes.py
+++ finalproject/app/routes.py
@@ -4,8 +4,8 @@ from urllib.parse import urlsplit
 from datetime import datetime, timezone
 from app import app, db
 import sqlalchemy as sa
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
+from app.models import User, Post
 
 
 
@@ -16,11 +16,17 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'Alex'}
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'John'},
@@ -31,7 +37,8 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template("index.html", title='Home Page', posts=posts)
+    return render_template("index.html", title='Home Page', form=form, posts=posts)
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
