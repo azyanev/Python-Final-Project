57298874ffbe1c42add7ddfd67e7a808ddf7bc65
AZYanev18 <azyanev@uni-sofia.bg>
2025-02-04 22:32:30 +0200

    improving user model

--- finalproject/app/routes.py
+++ finalproject/app/routes.py
@@ -1,10 +1,19 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from urllib.parse import urlsplit
+from datetime import datetime, timezone
 from app import app, db
 import sqlalchemy as sa
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
+
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
 
 
 @app.route('/')
