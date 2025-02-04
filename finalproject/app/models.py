13b29029c8ce7034dcd01b4a9e2f90054e51ad05
AZYanev18 <azyanev@uni-sofia.bg>
2025-02-04 20:19:51 +0200

    adding password hashing and checking

--- finalproject/app/models.py
+++ finalproject/app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
@@ -13,6 +14,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
 
 class Post(db.Model):
